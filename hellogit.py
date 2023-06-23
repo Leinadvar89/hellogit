2fae62bb4e454819924c144eb21390163ccddc22
Leinadvar89 <sfxtecnologia@gmail.com>
2023-06-23 07:33:34 -0500

    Otro commit para ver como se ve un diff

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,4 @@
 print("PRUEBAS GIT")
 print("PRIMERA LINEA DE COMANDOS")
 print("Esta es la segunda linea para hacer un checkout con git")
+print("Esta es otra linea para hacer un diff y ver como se muestra que ha habido cambios")
